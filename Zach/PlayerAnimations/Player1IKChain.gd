4d78e1f5d108ac66c34d9ea981a43d0cf8c51a7e
ZacharyLawrence13 <39809819+ZacharyLawrence13@users.noreply.github.com>
2019-11-08 18:20:03 -0500

    walkiing anim done

--- Zach/PlayerAnimations/Player1IKChain.gd
+++ Zach/PlayerAnimations/Player1IKChain.gd
@@ -1,15 +1,19 @@
 extends Node2D
 
-var speed = 8
+var speed = 4
 
 func _ready():
 	$AnimationPlayer.current_animation = "idle"
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_right"):
+		$Particles2D.emitting = true
 		$AnimationPlayer.current_animation = "walking"
 	if Input.is_action_pressed("ui_right"):
 		position.x += speed
+	if Input.is_action_just_released("ui_right"):
+		$Particles2D.emitting = false
+		$AnimationPlayer.current_animation = "idle"
 
 func flip_character():
 	for node in get_children():
