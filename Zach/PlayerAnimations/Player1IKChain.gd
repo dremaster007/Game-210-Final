65cf098f26d411c543efa48cb6c00ba0a31e35b2
ZacharyLawrence13 <39809819+ZacharyLawrence13@users.noreply.github.com>
2019-11-08 15:48:56 -0500

    good enough

--- Zach/PlayerAnimations/Player1IKChain.gd
+++ Zach/PlayerAnimations/Player1IKChain.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
+var speed = 8
+
 func _ready():
-	yield(get_tree().create_timer(2), "timeout")
-	flip_character()
-	yield(get_tree().create_timer(2), "timeout")
-	flip_character()
+	$AnimationPlayer.current_animation = "idle"
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_right"):
+		$AnimationPlayer.current_animation = "walking"
+	if Input.is_action_pressed("ui_right"):
+		position.x += speed
 
 func flip_character():
 	for node in get_children():
