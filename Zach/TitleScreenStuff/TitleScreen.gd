a42a78a5955724d8fdf09605004ae0d2c1dc5235
ZacharyLawrence13 <39809819+ZacharyLawrence13@users.noreply.github.com>
2019-11-14 21:00:41 -0500

    almost a cool title screen

--- Zach/TitleScreenStuff/TitleScreen.gd
+++ Zach/TitleScreenStuff/TitleScreen.gd
@@ -1,17 +1,19 @@
-extends CanvasLayer
+extends Node2D
 
 export (String, FILE) var picking_scene
 
 func _ready():
 	OS.window_fullscreen = true
-	
-	$PlayButton.connect("pressed", self, "play_animation", ["play"])
 
-func play_animation(animation):
-	match animation:
-		"play":
-			$AnimationPlayer.play("change_scene")
+func play_animation(anim):
+	if anim == "to_picking_scene":
+		$TitleTransition.play_transition()
 
 func change_scene(new_scene):
 	get_tree().change_scene(new_scene)
-	#print(new_scene)
+
+func _on_PlayButton_pressed():
+	play_animation("to_picking_scene")
+
+func _on_TitleTransition_transition_finished():
+	change_scene(picking_scene)
---
+++ Zach/TitleScreenStuff/TitleTransition.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+signal transition_finished
+
+func play_transition():
+	print("adsada")
+	$AnimationPlayer.play("to_picking_screen")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "to_picking_screen":
+		emit_signal("transition_finished")
