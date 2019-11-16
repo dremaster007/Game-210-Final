924bdafef15c1765421c5f2519bd437434d383b5
dremaster007 <matosman1209@gmail.com>
2019-11-16 16:29:43 -0500

    Worked more on the picking screen. There is now a distinguish between which player is selecting which character

--- _FinalProject/Assets/MainScenes/PickingScreen/Scripts/PickingScreen.gd
+++ _FinalProject/Assets/MainScenes/PickingScreen/Scripts/PickingScreen.gd
@@ -9,11 +9,15 @@ onready var characters = [$MainBuilding/CharacterHolder/Character1,
 						$MainBuilding/CharacterHolder/Character7,
 						$MainBuilding/CharacterHolder/Character8]
 
-var char_selected = null
-var selected_num_1 = 0
-var selected_num_2 = 0
-var selected_num_3 = 0
-var selected_num_4 = 0
+export (PackedScene) var PickingScreenPlayer
+
+
+var char_selected_1 = null
+var char_selected_2 = null
+var char_selected_3 = null
+var char_selected_4 = null
+
+var selected_number = 0
 
 var first_row_min = 0
 var first_row_max = 3
@@ -23,67 +27,38 @@ var second_row_max = 7
 var Global = null
 
 func _ready():
-	if get_parent().get_parent() != null:
-		Global = get_parent().get_parent()
-	cycle_characters()
+	Global = find_parent("Global")
+	get_players()
 
-func _process(delta):
-	get_input()
+func get_players():
+	for player in $PlayerCounter.get_children():
+		player.queue_free()
+	
+	for player_num in Global.number_of_players:
+		var i = PickingScreenPlayer.instance()
+		i.player_number = player_num + 1
+		$PlayerCounter.add_child(i)
+		cycle_characters(i.player_number, selected_number)
 
-func get_input():
-	#----------------------------------------------------------
-	# GETTING THE NUMBER OF THE CHARACTER THAT WILL BE SELECTED
-	if Input.is_action_just_pressed("left_1"):
-		# We are doing some basic math to make sure that we can scroll off screen and not
-		# go down to the next row. We go back to the start of the row we are on.
-		selected_num_1 -= 1
-		if selected_num_1 == first_row_min - 1:
-			selected_num_1 = first_row_max
-		elif selected_num_1 == second_row_min - 1:
-			selected_num_1 = second_row_max
-		cycle_characters()
+func cycle_characters(player_number, selected_num):
+	#for i in characters:
+		#i.get_surface_material(0).set_albedo(Color(1,1,1,1))
 	
-	elif Input.is_action_just_pressed("right_1"):
-		# Same reason for when we press left
-		selected_num_1 += 1
-		if selected_num_1 == first_row_max + 1:
-			selected_num_1 = first_row_min
-		elif selected_num_1 == second_row_max + 1:
-			selected_num_1 = second_row_min
-		cycle_characters()
-	
-	# Up and Down are different because we can save lines of code by putting
-	# the overlapping code in a new if statement. This is a better practice.
-	elif Input.is_action_just_pressed("up_1"):
-		selected_num_1 -= 4
-	
-	elif Input.is_action_just_pressed("down_1"):
-		selected_num_1 += 4
-	
-	if Input.is_action_just_pressed("up_1") or Input.is_action_just_pressed("down_1"):
-		if selected_num_1 < first_row_min:
-			selected_num_1 += 8
-		elif selected_num_1 > second_row_max:
-			selected_num_1 -= 8
-		cycle_characters()
-	#----------------------------------------------------------
-
-func cycle_characters():
-	# Set the character selected to the number in the array
-	char_selected = characters[selected_num_1]
-	# Set all other sprites in the array to look like they are unselected
-	for i in characters:
-		#print(i.get_surface_material(0))
-		i.get_surface_material(0).set_albedo(Color(1,1,1,1))
-	#print(char_selected)
-	
-	char_selected.get_surface_material(0).set_albedo(Color(0.5,0.5,0.5))
-	# Set the character select to show that they are selected
-	#char_selected.opacity = 0.5
-
-func _on_BackButton_input_event(camera, event, click_position, click_normal, shape_idx):
-	if event is InputEventMouseButton:
-		print("test")
-		if event.button_index == 1:
-			if event.pressed == false:
-				Global.change_scene("title_screen")
+	match player_number:
+		1:
+			# Set the character selected to the number in the array
+			char_selected_1 = characters[selected_num]
+			$SelectionSprites/Player1.translation.x = char_selected_1.translation.x
+			$SelectionSprites/Player1.translation.y = char_selected_1.translation.y
+		2:
+			char_selected_2 = characters[selected_num]
+			$SelectionSprites/Player2.translation.x = char_selected_2.translation.x
+			$SelectionSprites/Player2.translation.y = char_selected_2.translation.y
+		3:
+			char_selected_3 = characters[selected_num]
+			$SelectionSprites/Player3.translation.x = char_selected_3.translation.x
+			$SelectionSprites/Player3.translation.y = char_selected_3.translation.y
+		4:
+			char_selected_4 = characters[selected_num]
+			$SelectionSprites/Player4.translation.x = char_selected_4.translation.x
+			$SelectionSprites/Player4.translation.y = char_selected_4.translation.y
--- _FinalProject/Global.gd
+++ _FinalProject/Global.gd
@@ -4,9 +4,29 @@ export (PackedScene) var Title_Screen
 export (PackedScene) var Picking_Screen
 export (PackedScene) var Play_Screen
 
+var number_of_players = 1
+
 func _ready():
 	OS.window_fullscreen = true
+	Input.connect("joy_connection_changed", self, "_joy_connection_changed")
+	get_joypads()
 	change_scene(Title_Screen)
+
+func _joy_connection_changed(device_id, is_connected):
+	get_joypads()
+	#print("Device ", device_id, " connected_status = ", is_connected)
+
+func get_joypads():
+	number_of_players = 1
+	var joypads = Input.get_connected_joypads()
+	if joypads != []:
+		for item in joypads:
+			number_of_players += 1
+	print(find_node("Scenes").get_child(0))
+	
+	if find_node("Scenes").find_node("PickingScreen") != null:
+		find_node("PickingScreen").get_players()
+	print("Number of players: ", number_of_players)
 
 func _process(delta):
 	if Input.is_action_just_pressed("jump_1"):
